b8bdbdba179521cd8464b932ce8fb876df9732f0
homile <ssanho2@naver.com>
2022-02-13 18:40:43 +0900

    커뮤니티 사이트 제작

---
+++ Inventory(조민우, 박준오)/layout2.php
@@ -0,0 +1,19 @@
+﻿<!doctype html>
+<html lang="ko">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>요기보드</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo CSS_PATH . '/style.css'?>"/>
+</head>
+<body class="d-flex flex-column vh-100">
+<!------------ 2단 레이아웃 시작 (skin/layout2.php) --------------->
+<?php require_once('menu_main.php'); ?>
+<div class="container vh-100">
+<div class="row">
+
+  </div> <!-- col-md-3 -->
+  <!--- 본문 들어가는 자리 (skin/layout2.php) ---->
+  <div class="col-md-12 order-md-2">
+
---
+++ Inventory(조민우, 박준오)/menu_main.php
@@ -0,0 +1,29 @@
+﻿<!-- skin/menu_main.php 시작 -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark px-3">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" data-target="#navbars" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbars">
+            <ul class="navbar-nav ml-md-auto">
+                <!-- 상단 우측 로그인 링크 출력 -->
+                <?php
+                if (isset($_SESSION['uno'])) {
+                    if ($_SESSION['level']==10) {
+                        echo '<li class="nav-item"><a class="nav-link" href="' .USER_PATH. '/admin.php">관리자</a></li>' ;
+                        echo '<li class="nav-item"><a class="nav-link" href="' .USER_PATH. '/logout.php">로그아웃</a></li>' ;
+                    } else {
+                        echo '<li class="nav-item"><a class="nav-link" href="' .USER_PATH. '/index.php">정보변경</a></li>' ;
+                        echo '<li class="nav-item"><a class="nav-link" href="' .USER_PATH. '/logout.php">로그아웃</a></li>' ;
+                    }
+                } else {
+                    echo '<li class="nav-item"><a class="nav-link" href="' .USER_PATH. '/login.php">로그인</a></li>' ;
+                }
+                ?>
+                <!-- 상단 우측 회원가입 링크 출력 -->
+                <?php
+                    echo '<li class="nav-item"><a class="nav-link" href="' .USER_PATH. '/signup.php">회원가입</a></li>' ;
+                ?>
+           </ul>
+  </div>
+</nav>
+<!-- skin/menu_main.php 끝 -->
